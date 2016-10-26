cbbc6a389ec2f6b8ab43ad1113bf72443bfd59b1
Kelvin Li <kelvinkfli@gmail.com>
2016-10-25 23:42:51 -0400

    added use of NYT api + comments

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -2,23 +2,29 @@ myApp = {};
 myVars = {};
 myEvents = {};
 
+//global variables to be accessed
 myVars.new_user_input = null;
 myVars.author_id = null;
 myVars.books = null;
+myVars.bestAuthorArray = null;
+myVars.dupedAuthorArray = [];
+myVars.uniqueAuthorArray = [];
 
+/* Functions to be Used */
 myApp.getUserInput = function(){
     var user_input = $('input').val(); //variable to store the value of whatever the user inputs
     myVars.new_user_input = user_input.replace(/\s/g, ''); //variable to store user_input WITHOUT any spaces
     console.log(myVars.new_user_input);
-};
-myApp.getAuthorID = function(){
+}; //retrieves user input and removes all spaces
+
+myApp.getAuthorID = function(new_input){
 
     $.ajax({ //ajax call through hackeryou proxy to retrieve the author ID given new_user_input
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
         method:'GET',
         data: {
-            reqUrl: `https://www.goodreads.com/api/author_url/${myVars.new_user_input}`,
+            reqUrl: 'https://www.goodreads.com/api/author_url/' + new_input,
             params: {
                 key: 'zaTX6u6bYmPadLvnD2VkaA',
             },
@@ -26,11 +32,11 @@ myApp.getAuthorID = function(){
         }
     }).then(function(res) {
         console.log(res);
-        myVars.author_id = res.GoodreadsResponse.author.id //store the author ID into a global variable to be accessed later
-        myApp.getBooks();
+        var author_id = res.GoodreadsResponse.author.id; //store the author ID into a global variable to be accessed later
+        myApp.getBooks(author_id);
     });
-};
-myApp.getBooks = function(){
+}; //gets author ID + prints book image to page
+myApp.getBooks = function(authorID){
     $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -39,7 +45,7 @@ myApp.getBooks = function(){
             reqUrl: `https://www.goodreads.com/author/list.xml`,
             params: {
                 key: 'zaTX6u6bYmPadLvnD2VkaA',
-                id: `${myVars.author_id}`,
+                id: authorID,
             },
             xmlToJSON: true,
         }
@@ -50,8 +56,7 @@ myApp.getBooks = function(){
         myApp.getBookImage();
     });
     console.log('This should come before goodreads object');
-};
-
+}; //gets all the author's books
 myApp.getBookImage = function(){
     $('#book-list').empty();
     for (i = 0; i < myVars.books.length; i++) {
@@ -59,28 +64,64 @@ myApp.getBookImage = function(){
         var bookTitle = myVars.books[i].title;
         $('#book-list').append('<li><img src=' + imageUrl + '><br><p>' + bookTitle + '</p></li>');
     }
-}
+}; //gets book image URL
+myApp.displayNYT = function() {
+    $.ajax({
+        dataType: 'json',
+        url: "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=test&offset=60",
+        method: 'GET',
+    }).then(function (res) {
+        myVars.bestAuthorArray = res.results;
 
+        for (i = 0; i < myVars.bestAuthorArray.length; i++){
+            var authorName = myVars.bestAuthorArray[i].author;
+            // console.log(authorName);
+            myVars.dupedAuthorArray.push(authorName);
+        }
 
+        console.log(myVars.dupedAuthorArray);
+        myVars.uniqueAuthorArray = myVars.dupedAuthorArray.filter(function(item, position){
+            return myVars.dupedAuthorArray.indexOf(item) == position;
+        });
+        console.log(myVars.uniqueAuthorArray);
+    });
+}; //gets authors from NYT api
+myApp.printAuthors = function(array){
+    for (i = 0; i < array.length; i++) {
+        $('#author-list').append('<li class="author-list-item"><a href="#">' + array[i] + '</a></li>');
+    }
+}; //prints best-selling authors to page
 
-
-
-
-
-
+//event handlers for web app
 myEvents.onSubmit = function(){
     $('#submit-button').on('click', function(){
         myApp.getUserInput();
-        myApp.getAuthorID();
+        myApp.getAuthorID(myVars.new_user_input);
     })
-}
+}; //when submitted, grab user input + display books
+myEvents.showAuthor = function(){
+    $('#show-authors').on('click', function(e){
+        e.preventDefault();
+        myApp.printAuthors(myVars.uniqueAuthorArray);
+    })
+}; //**TEMP** click to show author names
+myEvents.selectAuthor = function(){
+    $('#author-list').on('click', 'a', function(){
+        var authorClicked = $(this).text().replace(/\s/g, '');
+        console.log(authorClicked);
+        myApp.getAuthorID(authorClicked);
+    })
+}; //when author name is clicked, display books
 
+//initiatize other methods
 myApp.init = function(){
     myEvents.onSubmit();
-    // myEvents.onViewBooks();
-}
+    myEvents.showAuthor();
+    myEvents.selectAuthor();
+    myApp.displayNYT();
+};
 
-//This stuff runs on document ready
+//Run on document ready
 $(function(){
     myApp.init();
 })
